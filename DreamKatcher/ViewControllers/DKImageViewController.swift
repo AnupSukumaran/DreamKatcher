d8e4b13c497e5e06fe1c86bf41a1f553408dc59b
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-08-20 01:48:19 +0530

    - image adding process

---
+++ DreamKatcher/ViewControllers/DKImageViewController.swift
@@ -0,0 +1,49 @@
+//
+//  DKImageViewController.swift
+//  DreamKatcher
+//
+//  Created by Sukumar Anup Sukumaran on 20/08/20.
+//  Copyright © 2020 Tech_Tonic. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class DKImageViewController: UIViewController, StoryboardProtocol {
+
+    @IBOutlet weak var imgView: UIImageView!
+    
+    static var sceneStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    
+    var index: Int!
+    fileprivate var feed: ResultsData!
+    fileprivate var isPlaying: Bool!
+    
+    static func instantiate(feed: ResultsData, andIndex index: Int, isPlaying: Bool = false) -> UIViewController {
+        let viewController = DKImageViewController.instantiate()
+        viewController.feed = feed
+        viewController.index = index
+        viewController.isPlaying = isPlaying
+        return viewController
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeFeed()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    fileprivate func initializeFeed() {
+        
+        guard let urlStr = feed.formatted_data?.image else {return}
+        
+        print("urlStr = \(urlStr)")
+        
+        imgView.sd_setImage(with: URL(string: urlStr)) { (img, error, cache, url) in
+            
+        }
+        
+    }
+
+}
