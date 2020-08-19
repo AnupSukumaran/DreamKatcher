85efb38c7a6ac936d55cafb4ce79b33f9eb36c37
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-08-20 01:58:25 +0530

    = fixing the image and video combination

--- DreamKatcher/ViewControllers/DKFeedPageViewController.swift
+++ DreamKatcher/ViewControllers/DKFeedPageViewController.swift
@@ -15,6 +15,8 @@ protocol FeedPageViewDelegate: class {
 class DKFeedPageViewController: UIPageViewController {
     
     fileprivate var presenter: FeedPagePresenterDelegate!
+    
+    var showImageOnly: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +26,21 @@ class DKFeedPageViewController: UIPageViewController {
         presenter.viewDidLoad()
     }
     
-
-
 }
 
 extension DKFeedPageViewController: FeedPageViewDelegate {
     func presentInitialFeed(_ feed: ResultsData) {
+        //showImageOnly = arc4random_uniform(2) == 0
+        if showImageOnly {
+            let viewController = DKImageViewController.instantiate(feed: feed, andIndex: 0, isPlaying: true) as! DKImageViewController
+            setViewControllers([viewController], direction: .forward, animated: false, completion: nil)
+        } else {
+            let viewController = DKFeedViewController.instantiate(feed: feed, andIndex: 0, isPlaying: true) as! DKFeedViewController
+            setViewControllers([viewController], direction: .forward, animated: false, completion: nil)
+        }
         
-        let viewController = DKFeedViewController.instantiate(feed: feed, andIndex: 0, isPlaying: true) as! DKFeedViewController
-        setViewControllers([viewController], direction: .forward, animated: false, completion: nil)
+//        let viewController = DKFeedViewController.instantiate(feed: feed, andIndex: 0, isPlaying: true) as! DKFeedViewController
+//        setViewControllers([viewController], direction: .forward, animated: false, completion: nil)
     }
 }
 
@@ -40,21 +48,48 @@ extension DKFeedPageViewController: UIPageViewControllerDataSource, UIPageViewCo
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard let indexedFeed = presenter.fetchPreviousFeed() else { return nil }
-        return DKFeedViewController.instantiate(feed: indexedFeed.feed, andIndex: indexedFeed.index)
+        //showImageOnly = arc4random_uniform(2) == 0
+        if showImageOnly {
+            return DKImageViewController.instantiate(feed: indexedFeed.feed, andIndex: indexedFeed.index)
+        } else {
+            return DKFeedViewController.instantiate(feed: indexedFeed.feed, andIndex: indexedFeed.index)
+        }
+        
+        //return DKFeedViewController.instantiate(feed: indexedFeed.feed, andIndex: indexedFeed.index)
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         guard let indexedFeed = presenter.fetchNextFeed() else { return nil }
-        return DKFeedViewController.instantiate(feed: indexedFeed.feed, andIndex: indexedFeed.index)
+       // showImageOnly = arc4random_uniform(2) == 0
+        if showImageOnly {
+          return DKImageViewController.instantiate(feed: indexedFeed.feed, andIndex: indexedFeed.index)
+        } else {
+           return DKFeedViewController.instantiate(feed: indexedFeed.feed, andIndex: indexedFeed.index)
+        }
+        
+        //return DKFeedViewController.instantiate(feed: indexedFeed.feed, andIndex: indexedFeed.index)
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
         guard completed else { return }
-        if let viewController = pageViewController.viewControllers?.first as? DKFeedViewController, let previousViewController = previousViewControllers.first as? DKFeedViewController {
-            previousViewController.pause()
-            viewController.play()
-            presenter.updateFeedIndex(fromIndex: viewController.index)
+        //showImageOnly = arc4random_uniform(2) == 0
+        if showImageOnly {
+          if let viewController = pageViewController.viewControllers?.first as? DKImageViewController, let previousViewController = previousViewControllers.first as? DKImageViewController {
+              presenter.updateFeedIndex(fromIndex: viewController.index)
+          }
+        } else {
+           if let viewController = pageViewController.viewControllers?.first as? DKFeedViewController, let previousViewController = previousViewControllers.first as? DKFeedViewController {
+               previousViewController.pause()
+               viewController.play()
+               presenter.updateFeedIndex(fromIndex: viewController.index)
+           }
         }
+        
+//        if let viewController = pageViewController.viewControllers?.first as? DKFeedViewController, let previousViewController = previousViewControllers.first as? DKFeedViewController {
+//            previousViewController.pause()
+//            viewController.play()
+//            presenter.updateFeedIndex(fromIndex: viewController.index)
+//        }
     }
     
     
